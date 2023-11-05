51d2c1eb46268840ee6bb49057e3d32913eaca6b
Govind Kumar Yadav <robertgovind@gmail.com>
2023-11-05 20:47:04 +0545

    final

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -21,51 +21,89 @@ class _HomeState extends State<Home> {
   int seconds = 0;
   int miliseconds = 0;
 
+  Timer? timer;
+
+  bool isStarted = false;
+
   void startStopwatch() {
-    Timer.periodic(const Duration(milliseconds: 1), (Timer) {
+    timer = Timer.periodic(const Duration(milliseconds: 1), (Timer) {
       incrementMilisecond();
+      isStarted = true;
     });
   }
 
   void incrementMilisecond() {
     setState(() {
-      miliseconds++;
-      sMilisecond = miliseconds.toString();
+      if (miliseconds < 99) {
+        miliseconds++;
+        sMilisecond = miliseconds.toString();
+      } else {
+        incrementSecond();
+      }
 
       if (miliseconds < 10) {
         sMilisecond = '0' + sMilisecond;
       }
-      if (miliseconds > 1000) {
-        setState(() {
-          seconds++;
-          miliseconds = 0;
-        });
-        if (seconds < 10) {
-          sSeconds = '0' + sSeconds;
-        }
-        if (seconds <= 60) {
-          setState(() {
-            seconds++;
-            sSeconds = seconds.toString();
-          });
-        } else {
-          minutes++;
-          sMinutes = minutes.toString();
-          seconds = 0;
-        }
-        if (minutes < 10) {
-          sMilisecond = '0' + sMinutes;
-        }
+    });
+  }
+
+  void incrementSecond() {
+    setState(() {
+      if (seconds < 59) {
+        miliseconds = 0;
+        sMilisecond = '00';
+        seconds++;
+        sSeconds = seconds.toString();
+      } else {
+        incrementMinutes();
+      }
+
+      if (seconds < 10) {
+        sSeconds = '0' + sSeconds;
+      }
+    });
+  }
+
+  void incrementMinutes() {
+    setState(() {
+      if (seconds < 59) {
+        miliseconds = 0;
+        seconds = 0;
+        sMilisecond = '00';
+        sSeconds = '00';
+        minutes++;
+        sMinutes = minutes.toString();
+      } else {
+        miliseconds = 0;
+        seconds = 0;
+        minutes = 0;
+        sMilisecond = '00';
+        sSeconds = '00';
+        sMinutes = '00';
+      }
+
+      if (minutes < 10) {
+        sMinutes = '0' + sMinutes;
       }
     });
   }
 
   void resetStopwatch() {
     setState(() {
-      sMilisecond = '0';
-      sSeconds = '0';
-      sMinutes = '0';
+      timer?.cancel();
+      miliseconds = 0;
+      seconds = 0;
+      minutes = 0;
+      sMilisecond = '00';
+      sSeconds = '00';
+      sMinutes = '00';
+      isStarted = false;
     });
+  }
+
+  void pauseStopwatch() {
+    timer?.cancel();
+    isStarted = false;
   }
 
   @override
@@ -137,10 +175,14 @@ class _HomeState extends State<Home> {
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         ControlButton(
-                          text: 'Start',
-                          onPressed: () {
-                            startStopwatch();
-                          },
+                          text: isStarted ? 'Pause' : 'Start',
+                          onPressed: isStarted
+                              ? () {
+                                  pauseStopwatch();
+                                }
+                              : () {
+                                  startStopwatch();
+                                },
                         ),
                         ControlButton(
                           text: 'Reset',
